274776c9d2a6a05e7f3fc18cd6549a48d7b24679
Noman Kabeer <nomankabeerr@gmail.com>
2019-05-15 02:54:04 +0500

    migrations are done

---
+++ database/migrations/2019_05_14_200013_likes.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Likes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('post_id')->default(0);
+            $table->integer('user_id')->default(0);
+            $table->integer('like')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
