2430fdfa0ddba396aa901b250ee7dc5b870d0a4e
Noman Kabeer <nomankabeerr@gmail.com>
2019-11-10 15:05:40 +0500

    Working on repository design pattern. Added post repository.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,186 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Comment;
-use App\Follow;
-use App\Likes;
-use App\Post;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
-use App\Http\Controllers\Traits\UploadImage;
-use Illuminate\Support\Facades\Auth;
-
+use App\Repositories\PostRepository;
 class PostController extends Controller
 {
-    use UploadImage;
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    protected $postRepository = null;
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
     public function index()
     {
-        $posts = Post::get();
+        $posts = $this->postRepository->getPosts();
         return view('index' , compact('posts'));
     }
-
     public function store(Request $request)
     {
-        $image = $request->file('image');
-        $name = $this->uploadImage($image);
-        $request = $request->all();
-        Arr::forget($request , '_token');
-        Arr::forget($request , 'image');
-        $added_user_id = Arr::add( $request , 'user_id' , Auth::user()->id);
-        $added_image = Arr::add( $added_user_id, 'image' , $name);
-        Post::create($added_image);
+        $this->postRepository->storePost($request);
         return redirect()->route('index');
     }
      public function postDetails($id){
-        $post = Post::where('id', $id)->first();
+        $post = $this->postRepository->getPostDetail($id);
         return view('post_detail' , compact('post'));
      }
      public function LikeDislikePost(Request $request){
-
-         $value = $request->value;
-         $post_id = $request->post_id;
-         $post = Likes::where('user_id' , Auth::user()->id)->where('post_id' , $post_id);
-         if ($post->first() == null ){
-             Likes::create(['post_id' => $post_id , 'user_id' => Auth::user()->id , 'like' => $value]);
-             $total_dislike = $post->where('like' , 0)->count();
-             $total_like = $post->where('like' , 1)->count();
-             if($value == 1){
-                 $like_msg = 'Liked';
-                 $dislike_msg = 'Dislike This post';
-                 if($total_like >1){
-                     $other_like_msg = 'You and other '.$total_like.' people likes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
-                 }
-                 else{
-                     $other_like_msg = 'You liked this';
-                 }
-
-                 if($total_dislike > 0){
-                     $other_dislike_msg =  $total_dislike. ' people dislikes this';
-                 }
-                 else{
-                     $other_dislike_msg = 'No Dislikes';
-                 }
-
-             }
-             elseif($value == 0){
-
-                 $like_msg = 'Like This Post';
-                 $dislike_msg = 'Disliked';
-                 if($total_dislike > 1){
-                     $other_dislike_msg = 'You and other '.$total_dislike.' people dislikes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
-                 }
-                 else{
-                     $other_dislike_msg = 'You disliked this';
-                 }
-
-                 if($total_like > 0){
-                     $other_like_msg = ''.$total_like.' people likes this';
-                 }
-                 else{
-                     $other_like_msg = 'No likes';
-                 }
-             }
-                return [$like_msg , $other_like_msg , $dislike_msg , $other_dislike_msg];
-
-         }
-         elseif($post->where('like' , $value)->first() != null){
-             $post->delete();
-
-             $total_dislike = $post->where('like' , 0)->count();
-             $total_like = $post->where('like' , 1)->count();
-             $like_msg = 'Like This Post';
-             $dislike_msg = 'Dislike This post';
-             if($total_dislike > 0){
-                 $other_dislike_msg =  $total_dislike. ' people dislikes this';
-             }
-             else{
-                 $other_dislike_msg = 'No Dislikes';
-             }
-
-             if($total_like > 0){
-                 $other_like_msg = ''.$total_like.' people likes this';
-             }
-             else{
-                 $other_like_msg = 'No likes';
-             }
-             return [$like_msg , $other_like_msg , $dislike_msg , $other_dislike_msg];
-
-         }
-         else{
-            Likes::where('user_id' , Auth::user()->id)->where('post_id' , $post_id)->update(['like' => $value]);
-
-
-             $total_dislike = $post->where('like' , 0)->count();
-             $total_like = $post->where('like' , 1)->count();
-             if($value == 1){
-                 $like_msg = 'Liked';
-                 $dislike_msg = 'Dislike This post';
-                 if($total_like >1){
-                     $other_like_msg = 'You and other '.$total_like.' people likes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
-                 }
-                 else{
-                     $other_like_msg = 'You liked this';
-                 }
-
-                 if($total_dislike > 0){
-                     $other_dislike_msg =  $total_dislike. ' people dislikes this';
-                 }
-                 else{
-                     $other_dislike_msg = 'No Dislikes';
-                 }
-
-             }
-             elseif($value == 0){
-
-                 $like_msg = 'Like This Post';
-                 $dislike_msg = 'Disliked';
-                 if($total_dislike > 1){
-                     $other_dislike_msg = 'You and other '.$total_dislike.' people dislikes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
-                 }
-                 else{
-                     $other_dislike_msg = 'You disliked this';
-                 }
-
-                 if($total_like > 0){
-                     $other_like_msg = ''.$total_like.' people likes this';
-                 }
-                 else{
-                     $other_like_msg = 'No likes';
-                 }
-             }
-             return [$like_msg , $other_like_msg , $dislike_msg , $other_dislike_msg];
-
-
-         }
-
+        return $this->postRepository->LikeDislikePost($request);
      }
 
      public function AddComment(Request $request){
-        Comment::create(['post_id' => $request->post_id , 'user_id' => Auth::user()->id , 'comment' => $request->comment]);
-        return "success";
+        return $this->postRepository->addCommentToPost($request);
      }
     public function getComment($id){
-        return Comment::where('post_id' , $id)->orderBy('id' , 'desc')->get();
+        return $this->postRepository->getPostComments($id);
     }
 
     public function followUser(Request $request){
-        $follow_id = $request->follow_id;
-        $follow = Follow::where('user_id' , Auth::user()->id)->where('follow_id' , $follow_id);
-        if($follow->first() != null && $follow->first() != ''){
-            Follow::where('user_id' , Auth::user()->id)->where('follow_id' , $request->follow_id)->delete();
-            return 'follow';
-        }
-        else{
-            $request = $request->all();
-            Arr::forget($request , '_token');
-            Follow::create($request);
-            return 'following';
-        }
+      return $this->postRepository->followThisUser($request);
     }
 }
---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Comment;
+use App\Follow;
+use App\Likes;
+use App\Post;
+use Illuminate\Support\Arr;
+use App\Http\Controllers\Traits\UploadImage;
+use Illuminate\Support\Facades\Auth;
+
+class PostRepository
+{
+    use UploadImage;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getPosts()
+    {
+        return Post::get();
+    }
+
+    public function storePost($data)
+    {
+        $image = $data->file('image');
+        $name = $this->uploadImage($image);
+        $data = $data->all();
+        Arr::forget($data , '_token');
+        Arr::forget($data , 'image');
+        $added_user_id = Arr::add( $data , 'user_id' , Auth::user()->id);
+        $added_image = Arr::add( $added_user_id, 'image' , $name);
+        Post::create($added_image);
+        return redirect()->route('index');
+    }
+     public function getPostDetail($id){
+        return Post::where('id', $id)->first();
+     }
+     public function LikeDislikePost($data){
+
+         $value = $data->value;
+         $post_id = $data->post_id;
+         $post = Likes::where('user_id' , Auth::user()->id)->where('post_id' , $post_id);
+         if ($post->first() == null ){
+             Likes::create(['post_id' => $post_id , 'user_id' => Auth::user()->id , 'like' => $value]);
+             $total_dislike = $post->where('like' , 0)->count();
+             $total_like = $post->where('like' , 1)->count();
+             if($value == 1){
+                 $like_msg = 'Liked';
+                 $dislike_msg = 'Dislike This post';
+                 if($total_like >1){
+                     $other_like_msg = 'You and other '.$total_like.' people likes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
+                 }
+                 else{
+                     $other_like_msg = 'You liked this';
+                 }
+
+                 if($total_dislike > 0){
+                     $other_dislike_msg =  $total_dislike. ' people dislikes this';
+                 }
+                 else{
+                     $other_dislike_msg = 'No Dislikes';
+                 }
+
+             }
+             elseif($value == 0){
+
+                 $like_msg = 'Like This Post';
+                 $dislike_msg = 'Disliked';
+                 if($total_dislike > 1){
+                     $other_dislike_msg = 'You and other '.$total_dislike.' people dislikes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
+                 }
+                 else{
+                     $other_dislike_msg = 'You disliked this';
+                 }
+
+                 if($total_like > 0){
+                     $other_like_msg = ''.$total_like.' people likes this';
+                 }
+                 else{
+                     $other_like_msg = 'No likes';
+                 }
+             }
+                return [$like_msg , $other_like_msg , $dislike_msg , $other_dislike_msg];
+
+         }
+         elseif($post->where('like' , $value)->first() != null){
+             $post->delete();
+
+             $total_dislike = $post->where('like' , 0)->count();
+             $total_like = $post->where('like' , 1)->count();
+             $like_msg = 'Like This Post';
+             $dislike_msg = 'Dislike This post';
+             if($total_dislike > 0){
+                 $other_dislike_msg =  $total_dislike. ' people dislikes this';
+             }
+             else{
+                 $other_dislike_msg = 'No Dislikes';
+             }
+
+             if($total_like > 0){
+                 $other_like_msg = ''.$total_like.' people likes this';
+             }
+             else{
+                 $other_like_msg = 'No likes';
+             }
+             return [$like_msg , $other_like_msg , $dislike_msg , $other_dislike_msg];
+
+         }
+         else{
+            Likes::where('user_id' , Auth::user()->id)->where('post_id' , $post_id)->update(['like' => $value]);
+
+
+             $total_dislike = $post->where('like' , 0)->count();
+             $total_like = $post->where('like' , 1)->count();
+             if($value == 1){
+                 $like_msg = 'Liked';
+                 $dislike_msg = 'Dislike This post';
+                 if($total_like >1){
+                     $other_like_msg = 'You and other '.$total_like.' people likes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
+                 }
+                 else{
+                     $other_like_msg = 'You liked this';
+                 }
+
+                 if($total_dislike > 0){
+                     $other_dislike_msg =  $total_dislike. ' people dislikes this';
+                 }
+                 else{
+                     $other_dislike_msg = 'No Dislikes';
+                 }
+
+             }
+             elseif($value == 0){
+
+                 $like_msg = 'Like This Post';
+                 $dislike_msg = 'Disliked';
+                 if($total_dislike > 1){
+                     $other_dislike_msg = 'You and other '.$total_dislike.' people dislikes this';// , 'Dislike this post' , $total_dislike. ' people dislike this'];
+                 }
+                 else{
+                     $other_dislike_msg = 'You disliked this';
+                 }
+
+                 if($total_like > 0){
+                     $other_like_msg = ''.$total_like.' people likes this';
+                 }
+                 else{
+                     $other_like_msg = 'No likes';
+                 }
+             }
+             return [$like_msg , $other_like_msg , $dislike_msg , $other_dislike_msg];
+
+
+         }
+
+     }
+
+     public function addCommentToPost($data){
+        Comment::create(['post_id' => $data->post_id , 'user_id' => Auth::user()->id , 'comment' => $data->comment]);
+        return "success";
+     }
+    public function getPostComments($id){
+        return Comment::where('post_id' , $id)->orderBy('id' , 'desc')->get();
+    }
+
+    public function followThisUser($data){
+        $follow_id = $data->follow_id;
+        $follow = Follow::where('user_id' , Auth::user()->id)->where('follow_id' , $follow_id);
+        if($follow->first() != null && $follow->first() != ''){
+            Follow::where('user_id' , Auth::user()->id)->where('follow_id' , $data->follow_id)->delete();
+            return 'follow';
+        }
+        else{
+            $data = $data->all();
+            Arr::forget($data , '_token');
+            Follow::create($data);
+            return 'following';
+        }
+    }
+}
